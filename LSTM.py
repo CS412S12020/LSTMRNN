03ee83c9688bb3d900e47300bb8ee047e92e8f83
gary@rbf.gov.fj <gary@rbf.gov.fj>
2020-06-08 23:20:58 +1200

    Updated dataset & code
    Converted data via percentages and built model around this. Forecast horizon is set to 12 months.

--- LSTM.py
+++ LSTM.py
@@ -13,34 +13,29 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from numpy import concatenate
 from math import sqrt
+import pandas as pd
+import numpy as np
 
 #Implementation uses Long-Short Tem Memory Recurrent Neural Networks to make predictions
-#Left to do;
-#	Generate test set for Fiji for prediction of tourism numbers
-#	Test set will have tourism numbers as blanks with all other columns roughly the same
-#	How many steps into the future using predicted values as input to the next
-#	Add Oceania datasets
-#	Shuffle train datasets
+#Following code is sourced from below projects and configured for this model and dataset
 
-#Objective:
-#	How many months till normalization?
-#	Tourism numbers for coming year
+#Currently does a 12 month ahead prediction
 
 #See pages below
 #https://machinelearningmastery.com/use-different-batch-sizes-training-predicting-python-keras/
 #https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
 
 # convert series to supervised learning
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+def preprocess_timeseries(data, months_lag=1, future_steps=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
 	df = DataFrame(data)
 	cols, names = list(), list()
 	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
+	for i in range(months_lag, 0, -1):
 		cols.append(df.shift(i))
 		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
 	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
+	for i in range(0, future_steps):
 		cols.append(df.shift(-i))
 		if i == 0:
 			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
@@ -54,81 +49,93 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 		agg.dropna(inplace=True)
 	return agg
  
-series = read_excel('Master.xlsx')
-# pyplot.plot(series[:,1])
-# pyplot.title(series.columns[1], y=0.5, loc='right')
-# pyplot.show()
+series = read_excel(open('Master.xlsx','rb'), sheet_name='SwitchingColumns')
+
+forecast_horizon = 12
 x = series.values[:,1:20]
-data = preprocessing.normalize(x, axis=1, norm='l1')  #Normalize input data
-i = 0
-# plot each column
+
+i = 0 #counter for graph names
+
+# plot each feature in own graph
 pyplot.figure()
 for group in series.columns:
     if(group != 'Date'):
-       
-        pyplot.plot(data[:,i])
+        pyplot.plot(x[:,i])
         pyplot.title(group, y=0.5, loc='right')
-        pyplot.savefig(str(i) + "_"+ group + ".png",bbox_inches='tight',dpi=100)
-       
+        pyplot.savefig("Out\\Features\\" + str(i) + "_"+ group + ".png",bbox_inches='tight',dpi=100)
         pyplot.clf()
         i += 1
 
-# integer encode direction
-# encoder = LabelEncoder()
-# values[:,4] = encoder.fit_transform(values[:,4])
 # ensure all data is float
 x = x.astype('float32')
 # normalize features
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(x)
 
-# specify the number of lag months
-n_months = 6
-n_features = 19
-# frame as supervised learning
-reframed = series_to_supervised(scaled, n_months, 1)
+sliding_window_size = 4 # number for preceding time steps before prediction step
+n_features = 19 
+n_futuresteps = 1 #network output. Can explore further, if setting this to more than one will it predict multi-step-ahead?
+# preprocess data into samples using sliding window and step ahead number
+reframed = preprocess_timeseries(scaled, sliding_window_size, n_futuresteps)
+reframed.to_excel(r'Out\\Preprocessed_Data.xlsx', index = False)
 print(reframed.shape)
 
 # split into train and test sets
 values = reframed.values
-n_train_months = 178 #178 training data from other diseases and countries. 48 will be used for fiji
+n_train_months = 150 
 train = values[:n_train_months, :]
 test = values[n_train_months:, :]
 # split into input and outputs
-n_obs = n_months * n_features
+n_obs = sliding_window_size * n_features
 train_X, train_y = train[:, :n_obs], train[:, -n_features]
 test_X, test_y = test[:, :n_obs], test[:, -n_features]
-# train_X, train_y = train[:, :n_obs], train[:, 1]
-# test_X, test_y = test[:, :n_obs], test[:, 1]
-print(train_X.shape, len(train_X), train_y.shape)
-# reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], n_months, n_features))
-test_X = test_X.reshape((test_X.shape[0], n_months, n_features))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
-    
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], sliding_window_size, n_features))
+test_X = test_X.reshape((test_X.shape[0], sliding_window_size, n_features))
+
 # design network
 model = Sequential()
-model.add(LSTM(200, input_shape=(train_X.shape[1], train_X.shape[2]))) #100 number of neurons
+model.add(LSTM(300, input_shape=(train_X.shape[1], train_X.shape[2]))) #100 number of neurons
 model.add(Dense(1))
 # model.compile(loss='mae', optimizer='adam')
 model.compile(loss='mean_squared_error', optimizer='adam')
-
-
 # fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=20, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+history = model.fit(train_X, train_y, epochs=100, batch_size=1, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 # plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
+pyplot.plot(history.history['loss'], label='Cost Function Loss')
+pyplot.plot(history.history['val_loss'], label='Cross Validation Loss')
 pyplot.legend()
-pyplot.savefig("ActualVsPredictedLoss.png",bbox_inches='tight',dpi=100)  
+pyplot.savefig("Out\\LossValuesDuringTraining.png",bbox_inches='tight',dpi=100)  
 pyplot.show()
 
+# make predictions into the future using horizon
+yhat = model.predict(test_X) # predict first and take value as actual. Add to history
+#duplicate last sample from test set
+predictedValues = np.array([yhat[-1]])
 
+for x in range(forecast_horizon):
+	yhat = model.predict(test_X)
+	predictedValues = np.append(predictedValues, yhat[-1])
+	test_y = np.append(test_y,yhat[-1]) #add predicted value as actual for now
 
-# make a prediction
+	#duplicate last sample set
+	temp = np.copy(test_X[-1])
+	#shift data from sample, remove first, add new to last
+	temp = np.roll(temp, 1, axis=0)
+	test_X = np.insert(test_X,len(test_X) - 1,temp,0)
+	test_X[-1][sliding_window_size - 1][0] = yhat[-1] #set predicted value as history for next prediction
+
 yhat = model.predict(test_X)
-test_X = test_X.reshape((test_X.shape[0], n_months*n_features))
+predictedValues = np.append(predictedValues, yhat[-1])
+
+print('All Future Predicted Values')
+print('----------------------------------')
+print(str(predictedValues))
+print('----------------------------------')
+
+test_X = test_X.reshape((test_X.shape[0], sliding_window_size*n_features))
+
 # invert scaling for forecast
 inv_yhat = concatenate((yhat, test_X[:, -18:]), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
@@ -138,6 +145,25 @@ test_y = test_y.reshape((len(test_y), 1))
 inv_y = concatenate((test_y, test_X[:, -18:]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:,0]
-# calculate RMSE
+
+pyplot.clf()
+pyplot.plot(inv_y, label='actual')
+pyplot.plot(inv_yhat, label='predicted')
+pyplot.legend()
+pyplot.savefig("Out\\ActualVsPredictedValues.png",bbox_inches='tight',dpi=100)  
+pyplot.show()
+
+inv_yhat_df = pd.DataFrame(inv_yhat)
+inv_y_df = pd.DataFrame(inv_y)
+
+# print out all actual vs predicted values (inclusive of horizon steps into future)
+inv_yhat_df.to_excel(r'Out\\Predicted.xlsx', index = False)
+inv_y_df.to_excel(r'Out\\Actual.xlsx', index = False)
+
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+
+writeAccuracyFile = open("Out\\TESTRMSE.txt", "w")
+writeAccuracyFile.write('Test RMSE: %.3f' % rmse)
+writeAccuracyFile.close()
+
 print('Test RMSE: %.3f' % rmse)
